60eb3f302840c01d1c12bd4f8aadfcca1099c861
Repaxan <repaxan@gmail.com>
2014-10-20 20:35:50 -0700

    started work on models

--- CampusHelperApp/models.py
+++ CampusHelperApp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+STATE_CREATED = 1			# in this file?
+STATE_ACCEPTED = 2
+STATE_COMPLETED = 3
+
+class User(models.Model):
+   username = models.CharField(max_length = None, primary_key = True)
+   password = models.CharField(max_length = None)
+   email = models.EmailField(max_length = 254)
+   description = models.CharField(max_length = None)
+   # users.tasksCreated, users.tasksAccepted from Task ForeignKeys
+
+   def __str__(self):
+      return self.username
+
+class Task(models.Model):
+	id = models.AutoField(primary_key = True)				# don't specify this yourself (?)
+   title = models.CharField(max_length = None)
+   description = models.CharField(max_length = None)
+   creator = models.ForeignKey(User, related_name = "tasksCreated", on_delete = models.CASCADE)
+   acceptor = models.ForeignKey(User, related_name = "tasksAccepted", on_delete = models.PROTECT)
+   timePosted = models.DateTimeField(auto_now_add = True)		# set the field to the time when it's created
+   state = models.SmallIntegerField(default = STATE_CREATED)
+   notify = models.BooleanField(default = False)		# 	iteration 1?
+
+   def __str__(self):
+      return self.title
