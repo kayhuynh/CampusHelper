640a7fedc48a97703406fc8271d74bd531c9039f
Repaxan <repaxan@gmail.com>
2014-10-22 12:56:08 -0700

    trying to fix indents for nick?

--- CampusHelper/urls.py
+++ CampusHelper/urls.py
@@ -7,20 +7,19 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'CampusHelperApp.views.root'),
-    # url(r'^frontend.css$',TemplateView.as_view(template_name='frontend.css'))
-    url(r'^alltasks$', 'CampusHelperApp.views.alltasks'),
-    url(r'^^alltasks?q=(?P<query>\w+)$', 'CampusHelperApp.views.alltasksQuery'),
-    url(r'^profile$', 'CampusHelperApp.views.profile'),
-    url(r'^profile?q=(?P<helper>\w+)$', 'CampusHelperApp.views.profileQuery'),
-    url(r'^task$', 'CampusHelperApp.views.task'),
-    url(r'^task?q=(?P<taskID>\w+)$', 'CampusHelperApp.views.taskQuery'),
-    url(r'^newtask$', 'CampusHelperApp.views.newtask'),
-    url(r'^newuser$', 'CampusHelperApp.views.newuser'),
-    url(r'^mytasks$', 'CampusHelperApp.views.mytasks'),
-
-    )
+	# Examples:
+	# url(r'^$', 'mysite.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
+	url(r'^$', 'CampusHelperApp.views.root'),
+	# url(r'^frontend.css$',TemplateView.as_view(template_name='frontend.css'))
+	url(r'^alltasks$', 'CampusHelperApp.views.alltasks'),
+	url(r'^^alltasks?q=(?P<query>\w+)$', 'CampusHelperApp.views.alltasksQuery'),
+	url(r'^profile$', 'CampusHelperApp.views.profile'),
+	url(r'^profile?q=(?P<helper>\w+)$', 'CampusHelperApp.views.profileQuery'),
+	url(r'^task$', 'CampusHelperApp.views.task'),
+	url(r'^task?q=(?P<taskID>\w+)$', 'CampusHelperApp.views.taskQuery'),
+	url(r'^newtask$', 'CampusHelperApp.views.newtask'),
+	url(r'^newuser$', 'CampusHelperApp.views.newuser'),
+	url(r'^mytasks$', 'CampusHelperApp.views.mytasks'),
+)
 
--- CampusHelperApp/models.py
+++ CampusHelperApp/models.py
@@ -8,76 +8,76 @@ STATE_ACCEPTED = 2
 STATE_COMPLETED = 3
 
 class User(models.Model):
-   username = models.TextField(max_length = None, primary_key = True)
-   password = models.TextField(max_length = None)
-   email = models.EmailField(max_length = 254)
-   description = models.TextField(max_length = None)
-   cookieID = models.BigIntegerField(default = random.randint(-(2 ** 63), (2 ** 63) - 1), unique = True)
-   # self.tasksCreated, self.tasksAccepted from Task ForeignKeys
+	username = models.TextField(max_length = None, primary_key = True)
+	password = models.TextField(max_length = None)
+	email = models.EmailField(max_length = 254)
+	description = models.TextField(max_length = None)
+	cookieID = models.BigIntegerField(default = random.randint(-(2 ** 63), (2 ** 63) - 1), unique = True)
+	# self.tasksCreated, self.tasksAccepted from Task ForeignKeys
 
-   def __str__(self):
-      return "user: " + self.username
+	def __str__(self):
+		return "user: " + self.username
 
-   def markTaskCompleted(self, taskID):
-   	getTask(taskID).markCompleted()
+	def markTaskCompleted(self, taskID):
+		getTask(taskID).markCompleted()
 
-   def acceptTask(self, taskID):
-   	getTask(taskID).markAccepted(self)
+	def acceptTask(self, taskID):
+		getTask(taskID).markAccepted(self)
 
-   def postedTasks(self):
-   	return map(self.tasksCreated, lambda x: x.id)
+	def postedTasks(self):
+		return map(self.tasksCreated, lambda x: x.id)
 
-   def acceptedTasks(self):
-   	return map(self.tasksAccepted, lambda x: x.id)
+	def acceptedTasks(self):
+		return map(self.tasksAccepted, lambda x: x.id)
 
-   def setEmail(self, newEmail):
-   	self.email = newEmail
+	def setEmail(self, newEmail):
+		self.email = newEmail
 
-   def setPassword(self, newPass):
-   	self.password = newPass
+	def setPassword(self, newPass):
+		self.password = newPass
 
-   def setDescription(self, newDesc):
-   	self.description = newDesc
+	def setDescription(self, newDesc):
+		self.description = newDesc
 
 class Task(models.Model):
 	taskID = models.AutoField(primary_key = True)				# don't specify this yourself (?)
-   title = models.TextField(max_length = None)
-   description = models.TextField(max_length = None)
-   creator = models.ForeignKey(User, related_name = "tasksCreated", on_delete = models.CASCADE)
-   acceptor = models.ForeignKey(User, related_name = "tasksAccepted", on_delete = models.PROTECT, null = True, default = None)
-   timePosted = models.DateTimeField(auto_now_add = True)		# set the field to the time when it's created
-   state = models.SmallIntegerField(default = STATE_CREATED)
-   notify = models.BooleanField(default = False)		# 	iteration 1?
+	title = models.TextField(max_length = None)
+	description = models.TextField(max_length = None)
+	creator = models.ForeignKey(User, related_name = "tasksCreated", on_delete = models.CASCADE)
+	acceptor = models.ForeignKey(User, related_name = "tasksAccepted", on_delete = models.PROTECT, null = True, default = None)
+	timePosted = models.DateTimeField(auto_now_add = True)		# set the field to the time when it's created
+	state = models.SmallIntegerField(default = STATE_CREATED)
+	notify = models.BooleanField(default = False)		# 	iteration 1?
 
-   def __str__(self):
-      return "task: " + self.title
+	def __str__(self):
+		return "task: " + self.title
 
-   def markAccepted(self, acceptor):
-   	if self.state == STATE_CREATED:
-   		self.state = STATE_ACCEPTED
-   		self.acceptor = acceptor
-   	else:
-   		raise ValidationError("wrong state to be marked as accepted")
+	def markAccepted(self, acceptor):
+		if self.state == STATE_CREATED:
+			self.state = STATE_ACCEPTED
+			self.acceptor = acceptor
+		else:
+			raise ValidationError("wrong state to be marked as accepted")
 
-   def markCompleted(self):
-   	if self.state == STATE_ACCEPTED:
-   		self.state = STATE_COMPLETED
-   	else:
-   		raise ValidationError("wrong state to be marked as completed")
+	def markCompleted(self):
+		if self.state == STATE_ACCEPTED:
+			self.state = STATE_COMPLETED
+		else:
+			raise ValidationError("wrong state to be marked as completed")
 
-   def setTitle(self, newTitle):
-   	self.title = newTitle
+	def setTitle(self, newTitle):
+		self.title = newTitle
 
-   def setDescription(self, newDesc):
-   	self.description = newDesc
+	def setDescription(self, newDesc):
+		self.description = newDesc
 
-   def notify(self):
-   	self.notify = True
+	def notify(self):
+		self.notify = True
 
 def newUser(username, password, email, desc):
-   k = User(username = username, password = password, email = email, description = desc)
-   k.save()
-   return k
+	k = User(username = username, password = password, email = email, description = desc)
+	k.save()
+	return k
 
 def getUser(username):
 	return User.objects.get(username__exact = username)
