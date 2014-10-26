1df871e896b49c66ecaef13aa1cab0eef16f3be0
Repaxan <repaxan@gmail.com>
2014-10-25 22:02:45 -0700

    added a missing KeyError catcher

--- CampusHelperApp/views.py
+++ CampusHelperApp/views.py
@@ -59,14 +59,17 @@ def login(request):
 
 # Shows a list of all tasks globally 
 def alltasks(request):
-    if request.method == "GET":
-        cookieID = request.session["cookieID"]
-        user = models.getUserByCookieID(cookieID)
-        template = loader.get_template("postBoard/alltasks.html")
-        all_tasks = models.Task.objects.all()
-        context = Context({"allTasks": all_tasks, "user": user.username})
-        return HttpResponse(template.render(context))
-    else:
+    try:
+        if request.method == "GET":
+            cookieID = request.session["cookieID"]
+            user = models.getUserByCookieID(cookieID)
+            template = loader.get_template("postBoard/alltasks.html")
+            all_tasks = models.Task.objects.all()
+            context = Context({"allTasks": all_tasks, "user": user.username})
+            return HttpResponse(template.render(context))
+        else:
+            return FAILURE500
+    except (ValueError, KeyError):
         return FAILURE500
 
 def alltasksQuery(request, query):
